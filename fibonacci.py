d05b2f4171a02f24eea9c16b55b43b9764dcae0e
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2023-07-06 22:38:22 -0700

    on agent finish

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib = [0, 1]
+        a, b = 0, 1
+        for _ in range(n-2):
+            a, b = b, a + b
+            fib.append(b)
+        return fib
+
+print(fibonacci(10))print(fibonacci(10))
