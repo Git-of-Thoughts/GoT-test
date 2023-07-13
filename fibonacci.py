1fa2a3b8f4cf2e1dfb7bca7fc1f7778ae433eb36
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-12 20:57:22 -0700

    on agent finish

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib = [0, 1]
+        while len(fib) < n:
+            fib.append(fib[-1] + fib[-2])
+        return fib
+
+print(fibonacci(10))
