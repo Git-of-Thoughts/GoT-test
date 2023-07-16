43eb369294375ad2a14eee3c9000505b19f6fa22
Your Name <you@example.com>
2023-07-16 06:12:50 +0000

    on agent finish

---
+++ snake_game.py
@@ -0,0 +1,28 @@
+import pygame
+
+# Initialize the game
+pygame.init()
+
+# Set up the game window
+window_width = 800
+window_height = 600
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Snake Game')
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update the game
+
+    # Draw the game
+    window.fill((0, 0, 0))
+
+    # Refresh the display
+    pygame.display.update()
+
+# Quit the game
+pygame.quit()
