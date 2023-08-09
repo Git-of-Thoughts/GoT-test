e8ce6cb0458b3f63aa23cc902dd3ace28cb03a49
Your Name <you@example.com>
2023-08-09 11:57:50 +0000

    on llm start

---
+++ snake.py
@@ -0,0 +1,19 @@
+class Snake:
+    def __init__(self):
+        self.body = [(0, 0)]
+        self.directions = {'up': (-1, 0), 'down': (1, 0), 'left': (0, -1), 'right': (0, 1)}
+
+    @property
+    def head(self):
+        return self.body[0]
+
+    def move(self, direction):
+        new_head = (self.head[0] + self.directions[direction][0], self.head[1] + self.directions[direction][1])
+        if new_head in self.body:
+            return False
+        self.body.insert(0, new_head)
+        self.body.pop()
+        return True
+
+    def grow(self):
+        self.body.append(self.body[-1])
