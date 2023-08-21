2e9b1740ad7c6d17108f4784ccf1be6cc1bb9b81
Your Name <you@example.com>
2023-08-21 04:26:27 +0000

    on llm start

---
+++ game.py
@@ -0,0 +1,13 @@
+from player import Player
+from location import Location
+from score import Score
+
+class Game:
+    def __init__(self):
+        self.player = Player()
+        self.location = Location()
+        self.score = Score()
+
+    def start_game(self):
+        # Start the game
+        pass
