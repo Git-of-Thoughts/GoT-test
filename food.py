6315134302cac80a51280c56aee3644d6ba7a839
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2023-08-23 06:31:55 +0000

    on llm start

---
+++ food.py
@@ -0,0 +1,7 @@
+class Food:
+    def __init__(self):
+        self.position = (0, 0)
+
+    def place(self):
+        # Place the food at a random location
+        pass
---
+++ snake.py
@@ -0,0 +1,16 @@
+class Snake:
+    def __init__(self):
+        self.body = [(0, 0)]
+        self.direction = "RIGHT"
+
+    def move(self):
+        # Move the snake in the current direction
+        pass
+
+    def grow(self):
+        # Grow the snake
+        pass
+
+    def check_collision(self):
+        # Check if the snake has collided with itself
+        pass
